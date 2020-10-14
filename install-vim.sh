e77159d1aa45eba9c7c7eaa670e9786b4bdb5408
kentaro-a <minminzemi1121@yahoo.co.jp>
2020-10-14 12:33:22 +0900

    F

---
+++ install-vim.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+dir=$HOME/usr/bin; [ ! -e $dir ] && mkdir -p $dir
+if [ -e $dir/vim ]; then
+	echo "Already Exist vim in ${dir}/usr/bin"
+else
+	echo "Install vim in ${dir}/usr/bin"
+	echo 'export PATH=$HOME/usr/bin:$PATH' >> ~/.bashrc
+	cd /tmp/
+	git clone https://github.com/vim/vim.git
+	cd /tmp/vim
+	./configure --prefix=$HOME/usr
+	make
+	make install
+fi
+
+echo "You should execute"
+echo "$ source ~/.bashrc"
