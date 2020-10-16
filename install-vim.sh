66fccbddfed2587272744f01e2687cee7ab5636a
mac <mac@test.ne.jp>
2020-10-16 16:02:36 +0900

    F

--- install-vim.sh
+++ install-vim.sh
@@ -3,7 +3,7 @@ dir=$HOME/usr/bin; [ ! -e $dir ] && mkdir -p $dir
 if [ -e $dir/vim ]; then
 	echo "Already Exist vim in ${dir}/usr/bin"
 else
-	echo "Install vim in ${dir}/usr/bin"
+	echo "Install vim in ${dir}"
 	echo 'export PATH=$HOME/usr/bin:$PATH' >> ~/.bashrc
 	cd /tmp/
 	git clone https://github.com/vim/vim.git
